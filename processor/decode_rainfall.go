fe769814b0f2078b14406b47054ccd8058a95b19
Nathan Smith <nathan@nathansmith.io>
2025-03-25 21:27:29 -0700

    wip

--- processor/decode_rainfall.go
+++ processor/decode_rainfall.go
@@ -1,10 +1,14 @@
 package processor
 
 import (
+	"log/slog"
+
 	"github.com/nathanmsmith/rtldavis/protocol"
 )
 
-func DecodeRainfall(m protocol.Message)  {
+// I'm not totally sure how rainfall and rain rate differ yet.
+func DecodeRainfall(m protocol.Message) {
 	rainClicks := (m.Data[3] & 0x7F)
-	slog.Info("Rain click reading received", , "raw_byte_data", bytesToSpacedHex(m.Data)))
+	slog.Info("Rain click reading received", "raw_byte_data", bytesToSpacedHex(m.Data), "rainClicks", rainClicks)
+	return
 }
