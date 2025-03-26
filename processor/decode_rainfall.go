af5958c374cdb92b5b1e8cfd8e08703a6f1be2d8
Nathan Smith <nathan@nathansmith.io>
2025-03-25 21:28:03 -0700

    wip

--- processor/decode_rainfall.go
+++ processor/decode_rainfall.go
@@ -10,5 +10,4 @@ import (
 func DecodeRainfall(m protocol.Message) {
 	rainClicks := (m.Data[3] & 0x7F)
 	slog.Info("Rain click reading received", "raw_byte_data", bytesToSpacedHex(m.Data), "rainClicks", rainClicks)
-	return
 }
