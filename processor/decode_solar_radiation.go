3c9ff3bf66b284eae148d01e647cbeff9c1e6f7a
Nathan Smith <nathan@nathansmith.io>
2026-01-10 21:32:41 -0800

    wip

--- processor/decode_solar_radiation.go
+++ processor/decode_solar_radiation.go
@@ -7,27 +7,23 @@ import (
 	"github.com/nathanmsmith/rtldavis/protocol"
 )
 
-func DecodeSolarRadiation(m protocol.Message) (float32, error) {
-	// From Dekay (https://github.com/dekay/im-me/blob/master/pocketwx/src/protocol.txt):
-	// > Bytes 3 and 4 are solar radiation.  The first byte is MSB and the second LSB.
-	// > The lower nibble of the 4th byte is again always 5, so they only use the first
-	// > three nibbles.  A value of FF in the third byte indicates that no sensor is
-	// > present.
-	// > Solar radiation = (byte3 << 8 + byte4) >> 6) * 1.757936
-	// >
-	// > Reference:
-	// > http://www.wxforum.net/index.php?topic=18489.msg178506#msg178506
-	// > Reference:
-	// > http://www.wxforum.net/index.php?topic=18489.msg190548#msg190548
+func DecodeSolarVoltage(m protocol.Message) (float32, error) {
+	// The Davis Vantage Pro has a UV Index (0x04) and Solar Radiation (0x06) sensor.
+	// Dekay has documented both here: https://github.com/dekay/im-me/blob/master/pocketwx/src/protocol.txt
+	// The Vantage Vue has neither, but has a solar panel voltage sensor (0x07).
 	//
-	// Dario says the bit is 0x07?
+	// Dario documented the reading, but couldn't find the units/value.
 	// https://github.com/dcbo/ISS-MQTT-Gateway/blob/master/src/main.cpp
 	//
-	// 1 W/m2
+	// Kobuki divides the result by 300. The unit is still unclear
+	// https://github.com/kobuki/VPTools/blame/master/Examples/ISSRx/ISSRx.ino#L178-L181
+	//
+	// Another repo divides the result by 100:
+	// https://github.com/HydroSense/FeatherM0_Davis_ISS_rx/blob/4d0fa1d2adae59eabcc53f4300e2acf0090b87bd/FeatherM0_Davis_ISS_rx.ino#L235
 
-	slog.Info("Solar reading received", "raw_byte_data", bytesToSpacedHex(m.Data))
+	slog.Info("Solar voltage reading received", "raw_byte_data", bytesToSpacedHex(m.Data))
 	if GetMessageType(m) != 0x06 {
-		return -1, errors.New("message does not have solar reading")
+		return -1, errors.New("message does not have solar voltage reading")
 	}
 
 	if m.Data[3] == 0xFF {
